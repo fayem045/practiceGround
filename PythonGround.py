66b4264cebdebe7f227f30bb4dd8b564b3ebdffd
fayem045 <maniojoannafaye@gmail.com>
2026-06-01 12:22:26 +0800

    Added lists

--- PythonGround.py
+++ PythonGround.py
@@ -1,11 +1,27 @@
-#create an pyramid from a character.(f11 to run)
-char = input("Enter a character: ")
-for i in range(1,7):
-    print(char*i)
+# #create an pyramid from a character.(f11 to run)
+# char = input("Enter a character: ")
+# for i in range(1,7):
+#     print(char*i)
 
-print("")
-print("Play2")
+# print("")
+# print("Play2: pyramid")
 #create center pyramid
 char2 = input("Enter a Character: ")
 for i in range(1,8):
-    print((char2*i).center(20))
+    # print((char2*i).center(20))
+    spaces = 7 - i
+    characters = 2 * i  - 1
+    print(" " * spaces + char2 * characters + " " * spaces)
+    
+print("")
+print("Playe2: update the list")
+fruits = ["Apple", "orange", "Banana"]
+fruits.insert(2, "Lychee")
+#apply,orange,lychee,banana
+print(fruits)
+
+print(" ")
+print("play3: counting similarities in a list")
+students = ["angela", "nana", "Zilong", "nana"]
+#case sensitive
+print(students.count("nana"))
